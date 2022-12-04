551aef3173a543cc83d192adb196f6d4b5728f68
Emil Calonius <emil.calonius@tuni.fi>
2022-12-04 04:09:29 +0200

    add optional keyword option to gif command

--- commands/gif.js
+++ commands/gif.js
@@ -1,15 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require("fs")
 const fetch = require('node-fetch')
-const GIPHY_URL = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_TOKEN}&tag=&rating=g`
-
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("gif")
-    .setDescription("Replies with a random gif"),
+    .setDescription("Replies with a random gif")
+    .addStringOption(option =>
+			option
+				.setName("keyword")
+				.setDescription("The keyword to use when fetching random gif")),
   async execute(interaction) {
-    const response = await fetch(GIPHY_URL)
+    const tag = interaction.options.getString("keyword") ?? ""
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_TOKEN}&tag=${tag}&rating=g`
+    const response = await fetch(url)
     const data = await response.json()
 
     await interaction.reply(data.data.url)
